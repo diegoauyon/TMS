237d10ff212bf4f39ac332c56595534da1394f07
diegoauyon <diegoauyon@gmail.com>
2017-03-07 18:42:13 -0600

    Initial Commit

---
+++ assets/helper.js
@@ -0,0 +1,32 @@
+function show_messages ( msg ) {
+  $( "#log_container" ).slideUp( 'slow', function () {
+    document.getElementById( "log" ).innerHTML = msg;
+    $( "#log_container" ).slideDown( 'slow', function () {
+    } );
+  } );
+}
+
+function load_code ( code ) {
+  if ( code.length >= 10 ) {
+    myCodeMirror.setValue( code );
+  }
+  else {
+    myCodeMirror.setValue( '//LOAD AN EXAMPLE TO TRY\n' +
+                           '//then load an input and click play\n\n' +
+                           '//Syntax:\n\n' +
+                           '//-------CONFIGURATION\n' +
+                           'name: [name_of_machine]\n' +
+                           'init: [initial_state]\n' +
+                           'accept: [accept_state_1],... ,[accept_state_n]\n\n' +
+                           '//-------DELTA FUNCTION:\n' +
+                           '[current_state],[read_symbol]\n' +
+                           '[new_state],[write_symbol],[>|<|-]\n\n' +
+                           '// < = left\n' +
+                           '// > = right\n' +
+                           '// - = hold\n' +
+                           '// use underscore for blank cells\n\n' +
+                           '//States and symbols are case-sensitive\n\n' +
+                           '//Load your code and click COMPILE.\n' +
+                           '//or load an example (top-right).\n' );
+  }
+}
